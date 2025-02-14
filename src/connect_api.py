47d4ecf09e2e9822e33e537102a8fe9d82987992
Katrin katryl-yh <katrin@rylander.eu>
2025-02-14 15:05:22 +0100

    test if read and write to database works with exchange data from API

--- src/connect_api.py
+++ src/connect_api.py
@@ -1,6 +1,8 @@
 from constants import COINMARKETCAP_API_KEY, EXHANGE_RATES_API_KEY, SYMBOLS
 from requests import Session
 from pprint import pprint
+from read_write_postgres import write_to_db, read_from_db
+import pandas as pd
 
 
 def make_request_cmc(session):
@@ -33,10 +35,21 @@ def make_request_exr(session):
 
 
 def get_data():
+    # 1. call function exchange data from exchange API
     s = Session()
-    data_cmc = make_request_cmc(s)
+    #data_cmc = make_request_cmc(s)
     data_exr = make_request_exr(s)
+    # TO DO: reshape the dictionary, so that we have 1 row with columns for each exchange rate + timestamp + data + base 
+    df = pd.DataFrame.from_dict(data_exr)
+    # 2. test write function with exchange data
+    table_nm = "exchange_rates"
+    write_to_db(df,table_nm)
+
+    # 3. test read function with exchange data
+    query = """SELECT * FROM exchange_rates;"""
+    return read_from_db(table_nm,query)
 
 
 if __name__ == "__main__":
-    get_data()
+    pprint(get_data())
+
