79077e05c724c0643ccbbf588f850937d6242f6f
svartfika <svartfika@proton.me>
2025-02-14 11:36:55 +0100

    load environment variables to constants, .env example

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,23 @@
-from dotenv import load_dotenv
+import dotenv
+import os
 
+dotenv.load_dotenv()
+
+# Loading enviorment variables with a default values in case of not present in .env file
+# os.getenv(<VARIABLE NAME>, <default value if not exists>)
+
+KAFKA_HOST = os.getenv("KAFKA_HOST", "localhost")
+KAFKA_PORT = os.getenv("KAFKA_PORT", 9092)
+
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", 5432)
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "")
+
+COINMARKETCAP_API_KEY = os.getenv("COINMARKETCAP_API_KEY", "")
+EXHANGE_RATES_API_KEY = os.getenv("EXHANGE_RATES_API_KEY", "")
+
+if __name__ == "__main__":
+    if not(COINMARKETCAP_API_KEY and EXHANGE_RATES_API_KEY):
+        raise ValueError("API keys not found.")
