c40917909a1d70ae09a44e46b5238a74aa802b50
svartfika <svartfika@proton.me>
2025-02-19 21:09:49 +0100

    added new constants

--- src/constants.py
+++ src/constants.py
@@ -8,18 +8,28 @@ dotenv.load_dotenv()
 
 KAFKA_HOST = os.getenv("KAFKA_HOST", "localhost")
 KAFKA_PORT = os.getenv("KAFKA_PORT", 9092)
+KAFKA_CONSUMER_GROUP = os.getenv("KAFKA_CONSUMER_GROUP", "coin_group")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "coin_topic")
 
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", 5432)
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "")
+POSTGRES_TABLE_EXCHANGE_RATES = os.getenv(
+    "POSTGRES_TABLE_EXCHANGE_RATES", "exchange_rates"
+)
+POSTGRES_TABLE_COIN_DATA = os.getenv("POSTGRES_TABLE_COIN_DATA", "crypto_data")
+
 
 COINMARKETCAP_API_KEY = os.getenv("COINMARKETCAP_API_KEY", "")
 EXHANGE_RATES_API_KEY = os.getenv("EXHANGE_RATES_API_KEY", "")
 
-SYMBOLS = os.getenv("SYMBOLS", "XRP,TRX")
+COINMARKETCAP_SYMBOLS = os.getenv("SYMBOLS", "XRP,TRX")
+EXCHANGE_RATES_BASE = os.getenv("EXCHANGE_RATES_BASE", "EUR")
+EXCHANGE_RATE_SYMBOLS = os.getenv("EXCHANGE_RATE_SYMBOLS", "DKK,NOK,SEK")
+
 
 if __name__ == "__main__":
-    if not(COINMARKETCAP_API_KEY and EXHANGE_RATES_API_KEY):
+    if not (COINMARKETCAP_API_KEY and EXHANGE_RATES_API_KEY):
         raise ValueError("API keys not found.")
