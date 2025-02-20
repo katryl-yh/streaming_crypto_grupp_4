fa32901c24ba63cd6b9c5a15327ffe06da65ab11
Katrin katryl-yh <katrin@rylander.eu>
2025-02-20 11:46:49 +0100

    initial setup of the dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,1 +1,15 @@
-import streamlit
+import streamlit as st
+import pandas as pd
+from read_write_postgres import read_from_db
+
+def layout():
+    
+    st.markdown("# Cryptocurrency data")
+    st.markdown("## Latest data")
+    
+        
+    st.markdown("## Bitcoin latest price in USD")
+
+
+if __name__ == "__main__":
+    layout()
